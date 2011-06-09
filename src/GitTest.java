0a5f67f400da78bf7eab08913e399931dc64ce90
PhilCarrier <pcarrier1@hotmail.com>
2011-06-09 13:28:21 -0400

    ouaip
    Change-Id: Idb80a1193e4c7b8baa592aaaba6f1e225989b219

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 		System.out.println("PEACE Yo! C'est moi le patronnnnn");
 		System.out.println("CRISSSSSTE");
 		System.out.println("DE TRABARNAK");
+		System.out.println("ouaip.");
 	}
 	
 	public void war()
