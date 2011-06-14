7b7960c6d51a57327d3c914e36093f50655169e9
PhilCarrier <pcarrier1@hotmail.com>
2011-06-14 14:53:37 -0400

    ça marche 5
    Change-Id: I3257559937d0f132355a7c991dc0db7b4cc40f66

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
 		System.out.println("PEACE Yo! C'est moi le patronnnnn");
 		System.out.println("Salut! Ça va?");
 		System.out.println("Ça va quand ça marche!");
+		System.out.println("Et là je pense qu'on y est enfin!");
 	}
 }
